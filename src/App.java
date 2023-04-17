76242fd3e7725e88fc213c17c4eaa11598a17091
Dan <98132259+Dan090807@users.noreply.github.com>
2023-04-17 12:41:50 -0600

    Now it reads files

--- src/App.java
+++ src/App.java
@@ -2,12 +2,14 @@ public class App {
 
     public static void main(String[] args) {
         
-        System.out.println("Diccionario Inglés - Español");
+        System.out.println("Diccionario Inglés - Español \n");
         
+        System.out.println("Leyendo archivo...");
+        new ProgressBar();
+        System.out.println("");
+
         Reader spanishReader = new Reader();
-
         spanishReader.setFile("Spanish.txt");
-
         System.out.println(spanishReader.read(spanishReader.getFile())); 
     }
 }
